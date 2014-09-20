ce7e82bab8d49ec74662fa5ab64fc155e88879dc
Tali Herzka <taliherzka@gmail.com>
2014-09-19 17:26:57 -0700

    created seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+5.times{User.create(username: Faker::Internet.user_name, password: Faker::Internet.password)}
+
+User.all.each do |user| 
+	user.questions << Question.create(title: Faker::Lorem.sentence, body: Faker::Lorem.sentence(1), user_id: user.id)
+end
+
+
+User.all.shuffle.each do |user|
+		Question.all.each do |q|
+			q.answers << Answer.create(body: Faker::Lorem.sentence(3), user_id: user.id)
+	end
+end
+
+User.all.each do |user|
+	Answer.all.shuffle.each do |a|
+		a.responses << Response.create(body: Faker::Lorem.sentence(3))
+	end
+end
